b308f69f73f282f2b110a38561792fead6869a38
apoch1 <48529742+apoch1@users.noreply.github.com>
2019-04-04 00:55:01 -0600

    Add files via upload

---
+++ includes/nav.php
@@ -0,0 +1,16 @@
+<?php
+	echo'<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-secondary border border-danger border-top-0 rounded-bottom">
+			<a class="navbar-brand" href="index.php"><img class="logo img-rounded rounded-circle" src="img/logo.png" style="width: 35px;"> Suehiro Home</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+					<a class="nav-item nav-link" href="menu.html">Menu</a>
+					<a class="nav-item nav-link" href="AboutUs.php">About Us</a>
+					<a class="nav-item nav-link" href="events.php">Weekly Events</a>
+					<a class="nav-item nav-link" href="social-media.php">Social Media</a>
+				</div>
+			</div>
+		</nav>';
+?>
